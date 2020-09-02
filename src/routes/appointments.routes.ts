bb9f675d969d6d2c08384e20de6e7be64d185c3a
Conrado <conradoms@gmail.com>
2020-09-02 08:40:53 -0300

    Implementação da classe de repositório de agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRespository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRespository();
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parsedDate = startOfHour(parseISO(date));
-    const findAppointmentInTheSameDate = appointments.find(appointment =>
-        isEqual(parsedDate, appointment.date)
-    );
+    const findAppointmentInTheSameDate = appointmentsRepository
+        .findByDate(parsedDate);
 
     if (findAppointmentInTheSameDate) {
         return response
@@ -20,9 +18,7 @@ appointmentsRouter.post('/', (request, response) => {
             .json({ message: 'This appointment is already booked!' });
     }
 
-    const appointment = new Appointment(provider, parsedDate);
-
-    appointments.push(appointment);
+    const appointment = appointmentsRepository.create(provider, parsedDate);
 
     return response.json(appointment);
 });
