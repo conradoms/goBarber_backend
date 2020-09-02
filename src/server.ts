434d5c71185556bc60f354048a9c0dd4abed7ecc
Conrado <conradoms@gmail.com>
2020-09-01 22:07:40 -0300

    Criação da primeira rota de agendamentos e separação do arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.post('/users', (request, response) => {
     const { name, email } = request.body;
