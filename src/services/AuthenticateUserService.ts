729957ebd65e2a0b92c143d911a741061ba7870d
Conrado <conradoms@gmail.com>
2020-09-05 21:17:48 -0300

    Implementação do JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface Request {
     email: string,
@@ -8,7 +9,7 @@ interface Request {
 }
 
 class AuthenticateUserService {
-    public async execute({ email, password }: Request): Promise<{ user: User }> {
+    public async execute({ email, password }: Request): Promise<{ user: User, token: string }> {
         const usersRepository = getRepository(User);
 
         const user = await usersRepository.findOne({
@@ -27,8 +28,16 @@ class AuthenticateUserService {
             throw new Error('Incorrect e-mail/password combination.');
         }
 
+        const token = sign({},
+            '8e5b5a9c695f118c8acb2cd49118b99a', // md5Online - usar para gerar a palavra secreta
+            {
+                subject: user.id, // Sempre o id do usuario
+                expiresIn: '1d'
+            });
+
         return {
             user,
+            token
         };
     }
 }
