3fa6a21b526bc96e7c5182b0457a654c200617e4
Conrado <conradoms@gmail.com>
2020-09-09 00:08:48 -0300

    Gravando o avatar no banco de dados

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
 
@@ -27,7 +28,21 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  return response.json({ ok: true });
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename
+    });
+
+    delete user.password;
+
+    return response.json(user);
+
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default usersRouter;
